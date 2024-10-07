885ee48f51ed02426600fec71395f7fdb9ea6a8b
GustavoBrites <52265505+GustavoBrites@users.noreply.github.com>
2024-10-07 15:43:55 +0000

    db, grafana, cli and docker app test

---
+++ med-conv-ai/rag.py
@@ -0,0 +1,147 @@
+import json
+
+from time import time
+
+from openai import OpenAI
+
+import data_ingestion
+
+
+client = OpenAI()
+
+index = data_ingestion.load_index()
+
+
+def search(query):
+    boost = {}
+
+    results = index.search(
+        query=query, filter_dict={}, boost_dict=boost, num_results=10
+    )
+
+    return results
+
+
+prompt_template = """
+You're a medical doctor. Answer the QUESTION based on the CONTEXT from our exercises database.
+Use only the facts from the CONTEXT when answering the QUESTION.
+
+QUESTION: {question}
+
+CONTEXT:
+{context}
+""".strip()
+
+entry_template = """
+question: {question}
+answer: {answer}
+""".strip()
+
+def build_prompt(query, search_results):
+    context = ""
+    
+    for doc in search_results:
+        context = context + entry_template.format(**doc) + "\n\n"
+
+    prompt = prompt_template.format(question=query, context=context).strip()
+    return prompt
+
+
+def llm(prompt, model="gpt-4o-mini"):
+    response = client.chat.completions.create(
+        model=model, messages=[{"role": "user", "content": prompt}]
+    )
+
+    answer = response.choices[0].message.content
+
+    token_stats = {
+        "prompt_tokens": response.usage.prompt_tokens,
+        "completion_tokens": response.usage.completion_tokens,
+        "total_tokens": response.usage.total_tokens,
+    }
+
+    return answer, token_stats
+
+
+rag_eval_prompt = """
+Imagine you are a seasoned quality assurance specialist for an advanced question-answering system. 
+Your expertise lies in assessing the precision and relevance of AI-generated responses. 
+Today, your mission is to scrutinize the correlation between a given question and its corresponding AI-generated answer.
+Based on the relevance of the generated answer, you will classify it
+as "NON_RELEVANT", "PARTLY_RELEVANT", or "RELEVANT".
+
+Here is the data for evaluation:
+
+Question: {question}
+Generated Answer: {answer_llm}
+
+Please analyze the content and context of the generated answer in relation to the question
+and provide your evaluation in parsable JSON without using code blocks:
+
+{{
+  "Relevance": "NON_RELEVANT" | "PARTLY_RELEVANT" | "RELEVANT",
+  "Explanation": "[Provide a brief explanation for your evaluation]"
+}}
+""".strip()
+
+
+def evaluate_relevance(question, answer):
+    prompt = rag_eval_prompt.format(question=question, answer=answer)
+    evaluation, tokens = llm(prompt, model="gpt-4o-mini")
+
+    try:
+        json_eval = json.loads(evaluation)
+        return json_eval, tokens
+    except json.JSONDecodeError:
+        result = {"Relevance": "UNKNOWN", "Explanation": "Failed to parse evaluation"}
+        return result, tokens
+
+
+def calculate_openai_cost(model, tokens):
+    openai_cost = 0
+
+    if model == "gpt-4o-mini":
+        openai_cost = (
+            tokens["prompt_tokens"] * 0.00015 + tokens["completion_tokens"] * 0.0006
+        ) / 1000
+    else:
+        print("Model not recognized. OpenAI cost calculation failed.")
+
+    return openai_cost
+
+
+def rag(query, model="gpt-4o-mini"):
+    start = time()
+
+    search_results = search(query)
+    prompt = build_prompt(query, search_results)
+    answer, token_stats = llm(prompt, model=model)
+
+    relevance, rel_token_stats = evaluate_relevance(query, answer)
+
+    finish = time()
+    took = finish - start
+
+    openai_cost_rag = calculate_openai_cost(model, token_stats)
+    openai_cost_eval = calculate_openai_cost(model, rel_token_stats)
+
+    openai_cost = openai_cost_rag + openai_cost_eval
+
+    answer_data = {
+        "answer": answer,
+        "model_used": model,
+        "response_time": took,
+        "relevance": relevance.get("Relevance", "UNKNOWN"),
+        "relevance_explanation": relevance.get(
+            "Explanation", "Failed to parse evaluation"
+        ),
+        "prompt_tokens": token_stats["prompt_tokens"],
+        "completion_tokens": token_stats["completion_tokens"],
+        "total_tokens": token_stats["total_tokens"],
+        "eval_prompt_tokens": rel_token_stats["prompt_tokens"],
+        "eval_completion_tokens": rel_token_stats["completion_tokens"],
+        "eval_total_tokens": rel_token_stats["total_tokens"],
+        "openai_cost": openai_cost,
+    }
+
+    return answer_data
